2e49594d7b09dbb72a88803c93276b7afd56dbc7
FedericaCampo97 <federica.campo97@gmail.com>
2023-08-23 14:49:04 +0200

    Ladder for exercise

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va stampato in forma umana (ovvero con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca nella documentazione di JS.
+*/
+
+//prompt x2
+//const
+//if/else
+//const
+//console.log
+
+//Chiedo all'utente quanti km vuole fare
+//Chiedo all'utente quanti anni ha
+//Calcolo il prezzo del biglietto in base ai km da percorrere
+//Applico gli sconti in base all'età del passeggiero
+//Stampo il prezzo finale 
